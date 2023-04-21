3fcda25fffaa23916d22853f544141a98c9384e0
Leshis <94430516+Leshis@users.noreply.github.com>
2023-04-21 14:40:16 +0100

    Added grid responsivness

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { v4 as uuidv4 } from 'uuid';
+
 const myLibrary = [];
 const libraryEl = document.getElementById('library');
 const newBookButton = document.getElementById('new-book');
+const deleteButton = document.getElementById('delete');
 const modal = document.getElementById('modal');
 const bookForm = document.getElementById('book-form');
 
 class Book {
-    constructor(title, author, pages, isRead, isFavourite) {
+    constructor(title, author, pages, isRead) {
         this.title = title;
         this.author = author;
         this.pages = pages;
         this.isRead = isRead;
-        this.isFavourite = isFavourite;
+        this.uuid = uuidv4();
     }
 
     getReadMsg = () => `${this.isRead ? 'Finished' : 'Not yet'} reading`;
@@ -24,19 +28,13 @@ function updateLibrary() {
     libraryEl.innerHTML = '';
     myLibrary.forEach((book) => {
         libraryEl.innerHTML += `
-            <div class="book">
-                <div class="book-desc">
-                    ${book.title} by ${book.author}, ${book.pages} pages
-                </div>
-                <div class="isRead">${book.getReadMsg()}</div>
-                <div class="favourite ${book.isFavourite}">&#9733;</div>
-            </div>
+             ${book}
         `;
     });
 }
 
-function addBookToLibrary(title, author, pages, isRead, isFavourite) {
-    myLibrary.push(new Book(title, author, pages, isRead, isFavourite));
+function addBookToLibrary(title, author, pages, isRead) {
+    myLibrary.push(new Book(title, author, pages, isRead));
     updateLibrary();
 }
 
@@ -48,17 +46,22 @@ function closeModal() {
     modal.style.display = 'none';
 }
 
+function handleDelete(e) {
+    console.log(e.target.id);
+    updateLibrary();
+}
+
 function handleSubmit(e) {
     e.preventDefault();
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
     const pages = document.getElementById('pages').value;
     const isRead = document.getElementById('is-read').checked;
-    const isFavourite = document.getElementById('is-favourite').checked;
-    addBookToLibrary(title, author, pages, isRead, isFavourite);
+    addBookToLibrary(title, author, pages, isRead);
     closeModal();
     e.target.reset();
 }
 
 newBookButton.addEventListener('click', openModal);
+deleteButton.addEventListener('click', handleDelete);
 bookForm.addEventListener('submit', handleSubmit);
